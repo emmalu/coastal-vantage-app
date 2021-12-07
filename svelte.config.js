0a9948946544d4df262aa4a8b07bf180c74fc67c
emmalu <emma.lu.paz@gmail.com>
2021-12-06 20:03:37 -0500

    svelte update config

--- svelte.config.js
+++ svelte.config.js
@@ -10,12 +10,12 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter()
-		/* adapter: adapter({
+		//adapter: adapter()
+		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: null
-		}) */
+		})
 	}
 };
 
