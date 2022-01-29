96d85d4c7859ddcfc56f58439299155c1c0082d9
emmalu <emma.lu.paz@gmail.com>
2022-01-29 17:01:15 -0500

    refactor contact info as mini component

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -2,6 +2,7 @@
 	import { browser, dev } from '$app/env';
 	import { Icon } from 'svelte-awesome';
 	import { globe } from 'svelte-awesome/icons';
+	import Contact from '$lib/contact.svelte';
 
 	// we don't need any JS on this page, though we'll load
 	// it in dev so that we get hot module replacement...
@@ -20,36 +21,7 @@
 	<title>Contact</title>
 </svelte:head>
 <div class="items-center bg-transparent py-3 flex flex-col justify-start sm:py-8">
-	<section>
-		<p class="font-semibold">Emma Paz</p>
-		<p class="pb-3">Licensed REALTOR&reg; with</p>
-	</section>
-	<section>
-		<img class="w-56" src="/logos/bhhs-cover.png" alt="BHHS Logo" />
-
-		<p class="pt-3">License #126299</p>
-		<!-- <p>1440 Ben Sawyer Blvd Ste 1503</p> -->
-		<p>774 S Shelmore Blvd Suite #106</p>
-		<p>Mt Pleasant, SC 29464</p>
-		<p class="pb-3"><a class="bhhs hover:text-red-700" href="tel:+18433888118">843-388-8118</a></p>
-		<a
-			href="https://emma.myhomecharleston.com"
-			target="_blank"
-			class="button shadow-md bg-blue-900 text-white hover:bg-blue-300 hover:text-blue-900 hover:text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 mb-3"
-			>BHHS Properties <svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="h-5 w-5 inline"
-				viewBox="0 0 20 20"
-				fill="currentColor"
-			>
-				<path
-					fill-rule="evenodd"
-					d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
-					clip-rule="evenodd"
-				/>
-			</svg>
-		</a>
-	</section>
+	<Contact />
 	<section class="max-w-xl mx-auto p-3 text-sm text-justify">
 		<div>
 			<span class="float-left text-6xl pr-2">E</span>mma first moved to Charleston in
