be79b30c0f19ffd91f5f5d099efefe78b5c2ced2
emmalu <emma.lu.paz@gmail.com>
2022-02-16 21:53:14 -0500

    sync blogs and lists to notion api

--- src/routes/bytes.svelte
+++ src/routes/bytes.svelte
@@ -1,39 +1,93 @@
 <script context="module">
-	import { browser, dev } from '$app/env';
+	import { browser } from '$app/env';
 	import Contact from '$lib/contact.svelte';
 	import { Icon } from 'svelte-awesome';
 	import { externalLink } from 'svelte-awesome/icons';
 
-	// we don't need any JS on this page, though we'll load
-	// it in dev so that we get hot module replacement...
-	export const hydrate = dev;
-
-	// ...but if the client-side router is already loaded
-	// (i.e. we came here from elsewhere in the app), use it
 	export const router = browser;
 
-	// since there's no dynamic data here, we can prerender
-	// it so that it gets served as a static asset in prod
-	export const prerender = true;
+	export async function load({ fetch }) {
+		const apiUrl = 'https://cv-notion.herokuapp.com';
+		const url = `${apiUrl}/posts`;
+		const res = await fetch(url, {
+			method: 'GET',
+			mode: 'cors',
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		if (res.ok) {
+			return {
+				props: {
+					posts: await res.json()
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load ${apiUrl}`)
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let posts = [];
 </script>
 
 <svelte:head>
 	<title>House Bytes</title>
 </svelte:head>
-<div class="flex flex-col py-2 sm:py-8">
-	<div class="text-center pb-28">
-		<h2>House Bytes [Blog]</h2>
-		<ul>
-			<li>
-				<a
-					href="https://emmalu.notion.site/NEW-in-2022-2e0906b7e6d9411bb9abc404abe10376"
-					target="_blank"
-					class="bhhs hover:text-red-700 text-3xl items-center"
-					>New in 2022
-					<Icon data={externalLink} />
-				</a>
-			</li>
-		</ul>
+
+<div class="flex flex-col py-2 sm:py-8 items-center">
+	<div class="text-center contents-center pb-16">
+		<h2 class="block pb-4 font-bold">House Bytes [Blog]</h2>
+		<p class="pb-4 w-96">
+			A place for timely, CHS-focused bits for homeowners, home buyers, & those interested in Real
+			Estate.
+		</p>
+		{#if posts.length === 0}
+			<p class="text-center animate-pulse">Loading... {posts.length} posts</p>
+		{:else if posts.length > 0}
+			<table class="w-full text-center">
+				<tbody>
+					{#each posts as post}
+						<tr>
+							<td class="items-center text-center">
+								<p>
+									<a
+										href="https://emmalu.notion.site/{post.slug}-{post.id.split('-').join('')}"
+										target="_blank"
+										class="bhhs hover:text-red-700 text-2xl items-center"
+										>{post?.title}
+										<Icon data={externalLink} /></a
+									>
+								</p>
+								{#each post.tags as tag}
+									<p
+										class="inline-block bhhs bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2"
+									>
+										{tag}
+									</p>
+								{/each}
+								<p class="text-xs italic">
+									Published {new Date(post.published).toLocaleDateString('en-us', {
+										year: 'numeric',
+										month: 'short',
+										day: 'numeric'
+									})}
+								</p>
+							</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		{:else}
+			<p class="text-red-400">
+				Unable to load posts at this time, but rest assured we're looking into it.. <br /> Apologies!
+			</p>
+		{/if}
 	</div>
 	<div>
 		<Contact />
---
+++ src/routes/lists.svelte
@@ -0,0 +1,83 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+	import Contact from '$lib/contact.svelte';
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	export async function load({ fetch }) {
+		const apiUrl = 'https://cv-notion.herokuapp.com';
+		const url = `${apiUrl}/lists`;
+		const res = await fetch(url, {
+			method: 'GET',
+			mode: 'cors',
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		if (res.ok) {
+			return {
+				props: {
+					lists: await res.json()
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load ${apiUrl}`)
+		};
+	}
+</script>
+
+<script>
+	export let lists = [];
+</script>
+
+<svelte:head>
+	<title>Curated Lists</title>
+</svelte:head>
+
+<div class="grid grid-flow-row py-2 sm:py-8 text-center">
+	<h2 class="block pb-4 font-bold">Curated Property Lists</h2>
+	{#if lists.length === 0}
+		<p class="text-gray-600">Loading lists...</p>
+	{:else if lists.length > 0}
+		<div
+			class="grid grid-rows-4 md:grid-rows-2 grid-flow-col justify-center items-center pb-8 gap-4"
+		>
+			{#each lists as list}
+				<div class="w-72 bg-white hover:bg-yellow-100 rounded-xl shadow-xl px-1 py-4">
+					<a href={list.url} target="_blank" class="items-center bhhs hover:text-red-700 text-2xl">
+						<p>
+							{list.title}
+							<!-- <Icon data={externalLink} /> -->
+						</p>
+						<p class="text-xs text-blue-900 pt-2 italic">
+							Updated: {new Date(list.edited).toLocaleDateString('en-us', {
+								year: 'numeric',
+								month: 'short',
+								day: 'numeric'
+							})}
+						</p>
+					</a>
+				</div>
+			{/each}
+		</div>
+	{:else}
+		<p class="text-red-400">
+			Unable to load lists at this time, but rest assured we're looking into it.. <br /> Apologies!
+		</p>
+	{/if}
+	<div>
+		<Contact />
+	</div>
+</div>
+
+<style>
+</style>
