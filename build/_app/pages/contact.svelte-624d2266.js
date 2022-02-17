7a89a3b292d736383ba609159b3be42983731f69
emmalu <emma.lu.paz@gmail.com>
2022-02-16 21:54:46 -0500

    publish with blogs and lists

---
+++ build/_app/pages/contact.svelte-624d2266.js
@@ -0,0 +1,31 @@
+import{S as st,i as ot,s as rt,I as it,M as lt,k as Q,e as i,j as U,t as s,T as ct,d as r,n as W,c as l,a as d,m as X,g as o,b as c,f as Y,o as Z,F as t,G as ht,x as tt,u as et,v as at}from"../chunks/vendor-b0dca1c4.js";import{C as mt}from"../chunks/contact-2deb9ba2.js";import{d as dt,b as ft}from"../chunks/env-a13806e5.js";function ut(nt){let p,h,f,x,g,e,v,S,$,_,k,I,b,j,A,y,N,R,T,P,q,m,G,u,M,E,O,B,C;return f=new mt({}),u=new it({props:{data:lt}}),{c(){p=Q(),h=i("div"),U(f.$$.fragment),x=Q(),g=i("section"),e=i("div"),v=i("span"),S=s("E"),$=s(`mma first moved to Charleston in
+			`),_=i("span"),k=s("2009"),I=s(`, to enroll in the M.S. Environmental Studies program
+			the
+			`),b=i("span"),j=s("College of Charleston"),A=s(`. Throughout her time in graduate
+			school, Emma learned the history & science of the
+			`),y=i("span"),N=s("Coastal Estuarine environment"),R=s(`
+			that is unique to our beautiful Lowcountry; this was the origin of her personal love for this place.
+			`),T=i("br"),P=i("br"),q=s(`
+			Over the years, Emma has pursued her career in
+			`),m=i("a"),G=s("Geographic Information Systems "),U(u.$$.fragment),M=s(`
+			& programming, taking on several opportunities to dig deeper into the geography of Charleston,
+			as well as some of the challenges we face as a community\u2014most notably regarding flooding in light
+			of Climate Change. To that end, Emma brings her technical skills & academic/professional background
+			in
+			`),E=i("span"),O=s("Resiliency & Sustainability"),B=s(` to the field of Real Estate,
+			with the intent of providing as much added value to her clients as she can.`),this.h()},l(n){ct('[data-svelte="svelte-rg684o"]',document.head).forEach(r),p=W(n),h=l(n,"DIV",{class:!0});var w=d(h);X(f.$$.fragment,w),x=W(w),g=l(w,"SECTION",{class:!0});var V=d(g);e=l(V,"DIV",{});var a=d(e);v=l(a,"SPAN",{class:!0});var F=d(v);S=o(F,"E"),F.forEach(r),$=o(a,`mma first moved to Charleston in
+			`),_=l(a,"SPAN",{class:!0});var z=d(_);k=o(z,"2009"),z.forEach(r),I=o(a,`, to enroll in the M.S. Environmental Studies program
+			the
+			`),b=l(a,"SPAN",{class:!0});var H=d(b);j=o(H,"College of Charleston"),H.forEach(r),A=o(a,`. Throughout her time in graduate
+			school, Emma learned the history & science of the
+			`),y=l(a,"SPAN",{class:!0});var J=d(y);N=o(J,"Coastal Estuarine environment"),J.forEach(r),R=o(a,`
+			that is unique to our beautiful Lowcountry; this was the origin of her personal love for this place.
+			`),T=l(a,"BR",{}),P=l(a,"BR",{}),q=o(a,`
+			Over the years, Emma has pursued her career in
+			`),m=l(a,"A",{href:!0,target:!0,class:!0});var L=d(m);G=o(L,"Geographic Information Systems "),X(u.$$.fragment,L),L.forEach(r),M=o(a,`
+			& programming, taking on several opportunities to dig deeper into the geography of Charleston,
+			as well as some of the challenges we face as a community\u2014most notably regarding flooding in light
+			of Climate Change. To that end, Emma brings her technical skills & academic/professional background
+			in
+			`),E=l(a,"SPAN",{class:!0});var K=d(E);O=o(K,"Resiliency & Sustainability"),K.forEach(r),B=o(a,` to the field of Real Estate,
+			with the intent of providing as much added value to her clients as she can.`),a.forEach(r),V.forEach(r),w.forEach(r),this.h()},h(){document.title="Contact",c(v,"class","float-left text-6xl pr-2"),c(_,"class","font-semibold"),c(b,"class","font-semibold"),c(y,"class","font-semibold"),c(m,"href","https://en.wikipedia.org/wiki/Geographic_information_system"),c(m,"target","_blank"),c(m,"class","bhhs hover:text-red-700 font-semibold"),c(E,"class","font-semibold"),c(g,"class","max-w-xl mx-auto p-3 text-sm text-justify"),c(h,"class","items-center bg-transparent py-3 flex flex-col justify-start sm:py-8")},m(n,D){Y(n,p,D),Y(n,h,D),Z(f,h,null),t(h,x),t(h,g),t(g,e),t(e,v),t(v,S),t(e,$),t(e,_),t(_,k),t(e,I),t(e,b),t(b,j),t(e,A),t(e,y),t(y,N),t(e,R),t(e,T),t(e,P),t(e,q),t(e,m),t(m,G),Z(u,m,null),t(e,M),t(e,E),t(E,O),t(e,B),C=!0},p:ht,i(n){C||(tt(f.$$.fragment,n),tt(u.$$.fragment,n),C=!0)},o(n){et(f.$$.fragment,n),et(u.$$.fragment,n),C=!1},d(n){n&&r(p),n&&r(h),at(f),at(u)}}}const _t=dt,bt=ft,yt=!0;class Et extends st{constructor(p){super();ot(this,p,null,ut,rt,{})}}export{Et as default,_t as hydrate,yt as prerender,bt as router};
